3676820325ff34cf15622a42563615bf8ee4998c
Ankush Malik <er.ankushmalik@gmail.com>
2023-08-10 13:34:48 +0530

    infinite-scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { faker } from "@faker-js/faker";
+import InfiniteScroll from "react-infinite-scroller";
+import "./App.css";
 
 function App() {
+  const data = new Array(1000).fill().map((value, index) => ({
+    id: index,
+    name: faker.name.firstName(5),
+    body: faker.lorem.paragraph(8),
+  }));
+  const showItems = (posts) => {
+    var items = [];
+    for (var i = 0; i < records; i++) {
+      items.push(
+        <div className="post" key={posts[i].id}>
+          <h3>{`${posts[i].name} - ${posts[i].id}`}</h3>
+          <p>{posts[i].body}</p>
+        </div>
+      );
+    }
+    return items;
+  };
+  const itemsPerPage = 20;
+  const [hasMore, setHasMore] = useState(true);
+  const [records, setrecords] = useState(itemsPerPage);
+  const loadMore = () => {
+    if (records === data.length) {
+      setHasMore(false);
+    } else {
+      setTimeout(() => {
+        setrecords(records + itemsPerPage);
+      }, 2000);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={loadMore}
+      hasMore={hasMore}
+      loader={<h4 className="loader">Loading...</h4>}
+      useWindow={false}
+    >
+      {showItems(data)}
+    </InfiniteScroll>
   );
 }
-
 export default App;
